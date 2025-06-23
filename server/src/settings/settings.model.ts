b4b22ae25ddfb2fbb9bed180e73afa649047ab08
alexjava92 <autoexpert37@yandex.ru>
2025-06-23 19:27:34 +0300

    add

--- server/src/settings/settings.model.ts
+++ server/src/settings/settings.model.ts
@@ -1,10 +1,25 @@
-import { Column, DataType, Model, Table } from 'sequelize-typescript';
+import {
+    Column,
+    DataType,
+    Model,
+    Table,
+    CreatedAt,
+    UpdatedAt,
+} from 'sequelize-typescript';
 
-@Table({ tableName: 'settings' })
+@Table({ tableName: 'settings', timestamps: true })
 export class SettingsModel extends Model<SettingsModel> {
     @Column({ type: DataType.STRING, primaryKey: true })
     key: string;
 
     @Column({ type: DataType.STRING })
     value: string;
+
+    @CreatedAt
+    @Column({ field: 'createdAt' })
+    createdAt: Date;
+
+    @UpdatedAt
+    @Column({ field: 'updatedAt' })
+    updatedAt: Date;
 }
