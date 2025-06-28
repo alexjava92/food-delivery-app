fa81ef56d7f0726b423789c42119637d6a4c0b6e
alexjava92 <autoexpert37@yandex.ru>
2025-06-28 14:00:28 +0300

    add

--- server/src/common/middleware/serverTimingMiddleware.ts
+++ server/src/common/middleware/serverTimingMiddleware.ts
@@ -4,14 +4,20 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class ServerTimingMiddleware implements NestMiddleware {
     use(req: Request, res: Response, next: NextFunction) {
-        const timings = [];
         const start = Date.now();
 
-        res.once('finish', () => {
+        // Сохраняем duration в переменную
+        res.on('finish', () => {
             const duration = Date.now() - start;
-            timings.push(`total;dur=${duration}`);
-            res.setHeader('Server-Timing', timings.join(', '));
+
+            // ✅ Заголовок устанавливаем, только если ответ не отправлен
+            if (!res.headersSent) {
+                res.setHeader('Server-Timing', `total;dur=${duration}`);
+            }
         });
+
+        // 👇 ставим Server-Timing заранее, он всё равно попадёт в ответ
+        res.setHeader('Server-Timing', 'processing');
 
         next();
     }
