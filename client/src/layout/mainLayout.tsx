7470808e9b72eae90f5736cf1d821864e032c78a
alexjava92 <autoexpert37@yandex.ru>
2025-07-07 16:00:39 +0300

    add

--- client/src/layout/mainLayout.tsx
+++ client/src/layout/mainLayout.tsx
@@ -1,7 +1,7 @@
 import React, {FC} from "react";
 import {useNavigate} from "react-router-dom";
 import {ArrowIconBack} from "../shared/images/icons/arrowIconBack";
-
+import classes from "./MainLayout.module.scss"
 import {Search} from "../entities/search/search";
 import {useTelegram} from "../hooks/useTelegram";
 
@@ -19,17 +19,21 @@ export const MainLayout: FC<IType> = ({children, heading, homePage, textCenter, 
     const navigate = useNavigate();
     return (
         <div className={tg?.colorScheme === 'light' ? 'container' : 'container darkTheme'}>
-            <h1 className={textCenter ? "h1 textCenter" : "h1"}>
+            <div className={classes.pageHeader}>
                 {!homePage &&
-                    <span className={"back"} onClick={() => navigate(-1)}>
-                    <ArrowIconBack/>
-                  </span>
+                    <span className={classes.back} onClick={() => navigate(-1)}>
+                <ArrowIconBack />
+            </span>
                 }
-                <span>{heading}</span>
-            </h1>
-            {isSearch && <Search url={'search?search'}/>}
+                <h1 className={`${classes.h1} ${textCenter ? classes.textCenter : ''}`}>
+                    {heading}
+                </h1>
+            </div>
+
+            {isSearch && <Search url={'search?search'} />}
             {children}
             <div className="menuBg"></div>
         </div>
+
     );
 };
