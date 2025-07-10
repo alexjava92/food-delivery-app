e60c0b9920a50defcb83ccd3fa7bce1a6bb3b5d0
alexjava92 <autoexpert37@yandex.ru>
2025-07-10 09:36:45 +0300

    add

--- client/src/entities/product/product.tsx
+++ client/src/entities/product/product.tsx
@@ -1,9 +1,10 @@
-import React, {FC, memo, useEffect, useState} from "react";
+import React, { FC, memo, useEffect, useState } from "react";
 import classes from "./product.module.scss";
-import {IProduct} from "../../types/types";
-import {FavoritesIcon} from "../../shared/images/icons/favoritesIcon";
-import {PlusAndMinus} from "../../shared/plusAndMinus/plusAndMinus";
-import {ImageZoomModal} from "../../shared/imageZoomModal/ImageZoomModal";
+import { IProduct } from "../../types/types";
+import { FavoritesIcon } from "../../shared/images/icons/favoritesIcon";
+import { PlusAndMinus } from "../../shared/plusAndMinus/plusAndMinus";
+import { useSpring, animated } from "@react-spring/web";
+import { useGesture } from "@use-gesture/react";
 
 interface IType {
     data: IProduct;
@@ -14,58 +15,65 @@ interface IType {
     count?: number;
 }
 
-export const Product: FC<IType> = memo(({data, inOrder, inCart, count, editAdmin, oneProduct}) => {
+export const Product: FC<IType> = memo(({ data, inOrder, inCart, count, editAdmin, oneProduct }) => {
+    const [favouritesProduct, setFavouritesProduct] = useState<IProduct[]>();
 
-    const [openZoom, setOpenZoom] = useState(false);
-
-    const classesArr = [classes.product]
-    if (oneProduct) classesArr.push(classes.oneProduct)
-    if (inCart || inOrder) classesArr.push(classes.small)
-
-    const [favouritesProduct, setFavouritesProduct] = useState<IProduct[]>()
+    const classesArr = [classes.product];
+    if (oneProduct) classesArr.push(classes.oneProduct);
+    if (inCart || inOrder) classesArr.push(classes.small);
 
     useEffect(() => {
-        const local = localStorage.getItem('food-delivery-favorites')
-        const favorites = JSON.parse(local ? local : '[]');
-        if (favorites) setFavouritesProduct(favorites)
+        const local = localStorage.getItem("food-delivery-favorites");
+        const favorites = JSON.parse(local ? local : "[]");
+        if (favorites) setFavouritesProduct(favorites);
     }, []);
 
     const toggleFavorite = (e: any) => {
-        e.preventDefault()
-        const favorites = JSON.parse(localStorage.getItem('food-delivery-favorites') || '[]');
+        e.preventDefault();
+        const favorites = JSON.parse(localStorage.getItem("food-delivery-favorites") || "[]");
         const isFavorite = favorites.find((product: IProduct) => product.id === data.id);
         if (isFavorite) {
             const updatedFavorites = favorites.filter((item: any) => item.id !== data.id);
-            localStorage.setItem('food-delivery-favorites', JSON.stringify(updatedFavorites));
-            setFavouritesProduct(updatedFavorites)
+            localStorage.setItem("food-delivery-favorites", JSON.stringify(updatedFavorites));
+            setFavouritesProduct(updatedFavorites);
         } else {
             const updatedFavorites = [...favorites, data];
-            localStorage.setItem('food-delivery-favorites', JSON.stringify(updatedFavorites));
-            setFavouritesProduct(updatedFavorites)
+            localStorage.setItem("food-delivery-favorites", JSON.stringify(updatedFavorites));
+            setFavouritesProduct(updatedFavorites);
         }
     };
 
+    const [{ scale }, api] = useSpring(() => ({ scale: 1 }));
+    const bind = useGesture(
+        {
+            onPinch: ({ offset: [s] }) => api.start({ scale: s }),
+            onWheel: ({ offset: [, s] }) => api.start({ scale: 1 + s / 100 }),
+            onDoubleClick: () => api.start({ scale: 1 }),
+        },
+        {
+            pinch: { scaleBounds: { min: 1, max: 3 }, rubberband: true },
+        }
+    );
+
     return (
-        <div className={classesArr.join(' ')}>
-            {
-                !data?.disabled &&
+        <div className={classesArr.join(" ")}>
+            {!data?.disabled && (
                 <>
                     <div className={classes.disabled}></div>
                     <span className={`error ${classes.textError}`}>
                         К сожалению данный товар временно отсутствует
                     </span>
                 </>
-            }
-
-            <div className={classes.image} onClick={() => setOpenZoom(true)}>
-                <img src={process.env.REACT_APP_API_URL + data?.image} alt={data?.title} />
-            </div>
-            {openZoom && (
-                <ImageZoomModal
-                    src={process.env.REACT_APP_API_URL + data?.image}
-                    onClose={() => setOpenZoom(false)}
-                />
             )}
+
+            <animated.div className={classes.image} {...bind()} style={{ touchAction: "none" }}>
+                <animated.img
+                    src={process.env.REACT_APP_API_URL + data?.image}
+                    alt={data?.title}
+                    style={{ scale }}
+                    className={classes.zoomImage} // можно стилизовать под нужные размеры
+                />
+            </animated.div>
 
             <div className={classes.box}>
                 <div className={classes.title}>
@@ -74,34 +82,29 @@ export const Product: FC<IType> = memo(({data, inOrder, inCart, count, editAdmin
                         <span className={classes.grams}>{data?.grams}</span>
                     </span>
 
-                    {
-                        (!editAdmin && !inCart && !inOrder) &&
+                    {!editAdmin && !inCart && !inOrder && (
                         <span onClick={toggleFavorite}>
                             <FavoritesIcon
-                                isActive={!!favouritesProduct?.find((product: IProduct) => product?.id === data?.id)}/>
+                                isActive={!!favouritesProduct?.find((product: IProduct) => product?.id === data?.id)}
+                            />
                         </span>
-                    }
-
-
+                    )}
                 </div>
 
                 <div className={classes.description}>{data?.description}</div>
 
-                {!inCart && !inOrder && (
-                    <div className={classes.price}>{data?.price} ₽</div>
-                )}
+                {!inCart && !inOrder && <div className={classes.price}>{data?.price} ₽</div>}
             </div>
 
-            {/* Цена для корзины */}
             {inCart && (
                 <div className={classes.priceBox}>
-                    <PlusAndMinus data={data}/>
+                    <PlusAndMinus data={data} />
                     <div className={classes.price}>
                         {data.count && data.count > 1 ? (
                             <>
                                 <span className={classes.subPrice}>
                                     {data.count} x {data.price}
-                                </span>{' '}
+                                </span>{" "}
                                 {data.count * +data.price} ₽
                             </>
                         ) : (
@@ -111,7 +114,6 @@ export const Product: FC<IType> = memo(({data, inOrder, inCart, count, editAdmin
                 </div>
             )}
 
-            {/* Цена для заказа */}
             {inOrder && (
                 <div className={classes.priceBox}>
                     {inOrder && <span className={classes.count}>x{data?.count}</span>}
@@ -120,7 +122,7 @@ export const Product: FC<IType> = memo(({data, inOrder, inCart, count, editAdmin
                             <>
                                 <span className={classes.subPrice}>
                                     {data.count} x {data.price}
-                                </span>{' '}
+                                </span>{" "}
                                 {data.count * +data.price} ₽
                             </>
                         ) : (
